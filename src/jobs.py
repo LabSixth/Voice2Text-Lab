2496ba3937cc9cc70b9debe06d48e59ab901b89d
YeeJunOw19 <yeejunow@gmail.com>
2025-05-20 12:43:43 -0500

    Metadata Extraction Pipeline
    Added Dagster pipeline to extract metadata from downloaded files and folders

--- src/jobs.py
+++ src/jobs.py
@@ -4,5 +4,8 @@ from dagster import define_asset_job
 # Define Dagster jobs
 run_download_pipeline = define_asset_job(
     name="run_download_pipeline",
-    selection=["download_data", "unpack_move", "clean_up"]
+    selection=[
+        "download_data", "unpack_move", "clean_up",
+        "file_structure_gather", "metadata_gather", "save_metadata"
+    ]
 )
