09155b9cf6267d9474095da6582a92889bf6bd61
YeeJunOw19 <yeejunow@gmail.com>
2025-05-22 21:48:42 -0500

    Modfied Asset
    Modified preprocessing asset with functions to read in artifacts instead of using dataframes from previous steps

--- src/data_ingestion/text_preprocessing.py
+++ src/data_ingestion/text_preprocessing.py
@@ -2,38 +2,76 @@
 import polars as pl
 import os
 import dagster as dg
+from pathlib import Path
 from src import global_configs as cf
+from src.data_ingestion.text_extraction import save_transcriptions
+from src.data_ingestion.metadata_extraction import save_metadata
 
 CONFIGS = cf.PIPELINE_CONFIG["Summarization_Named_Entity_Recognition"]
 
 
-@dg.asset(
-    ins={
-        "meta_df": dg.AssetIn(key="metadata_gather"),
-        "transcripts_df": dg.AssetIn(key="speech_to_text_conversion")
-    },
-    kinds={"python", "polars", "parquet"}
-)
-def create_full_dataset(meta_df: pl.DataFrame, transcripts_df: pl.DataFrame) -> None:
+def _read_data(filepath: Path) -> pl.DataFrame:
     """
-    Combines metadata and transcriptions into a single dataset, processes them by grouping
-    and aggregating specific columns, and saves the resulting dataset to a specified
-    directory in either Parquet or CSV format. The dataset is ordered based on user, chapter,
-    and recording IDs.
+    Reads a data file and returns it as a polars DataFrame. Determines the file type
+    based on the file extension and reads the data accordingly. Supports `.parquet`
+    and `.csv` file formats.
 
     Args:
-        meta_df (pl.DataFrame): A DataFrame containing metadata about the recordings.
-        transcripts_df (pl.DataFrame): A DataFrame containing transcriptions of the recordings.
+        filepath (Path): Path object representing the file to be read.
+
+    Returns:
+        pl.DataFrame: A polars DataFrame containing the data from the file.
+    """
+
+    # Get the file extension of the given file
+    file_ext = filepath.__str__().split(".")[-1]
+
+    if file_ext == "parquet":
+        df = pl.read_parquet(filepath)
+    else:
+        df = pl.read_csv(filepath)
+
+    return df
+
+
+@dg.asset(
+    deps=[save_transcriptions, save_metadata],
+    kinds={"python", "polars", "parquet"}
+)
+def create_full_dataset() -> None:
+    """
+    Combines metadata and transcription data into a single dataset and saves it in
+    the specified format and location. The function utilizes configurations for paths
+    and file details, ensures the output directory exists, and creates the dataset by
+    joining and processing the provided metadata and transcription files.
     """
 
     # Get configurations to for this task
     save_folder = cf.DATA_PATH.joinpath(CONFIGS["Folder_Tree"]["Combined_Data"]["Folder_Name"]).resolve()
     os.makedirs(save_folder, exist_ok=True)
 
+    meta_df = (
+        cf.DATA_PATH
+        .joinpath(
+            CONFIGS["Folder_Tree"]["Cleaned_Data"]["Folder_Nam"],
+            CONFIGS["Folder_Tree"]["Cleaned_Data"]["Metadata_File"]
+        )
+        .resolve()
+    )
+
+    transcripts_df = (
+        cf.DATA_PATH
+        .joinpath(
+            CONFIGS["Folder_Tree"]["Cleaned_Data"]["Folder_Nam"],
+            CONFIGS["Folder_Tree"]["Cleaned_Data"]["Transcription_File"]
+        )
+        .resolve()
+    )
+
     # Combine the metadata and transcriptions together into one dataset
     df = (
-        transcripts_df
-        .join(meta_df, on="id", how="left")
+        _read_data(transcripts_df)
+        .join(_read_data(meta_df), on="id", how="left")
         .sort("user_id", "chapter_id", "id")
         .group_by("user_id", "chapter_id", maintain_order=True)
         .agg(
